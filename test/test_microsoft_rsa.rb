0b382fb40fe4f55d4ae1d52fe211181614b04167
Jason Barnett <jason.w.barnett@gmail.com>
2017-04-08 11:51:26 -0400

    Initial commit to microsoft_rsa.

---
+++ test/test_microsoft_rsa.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestMicrosoftRsa < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end

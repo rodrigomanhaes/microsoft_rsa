91d98e4558614b3fb1e86fa1a33a8a21c47fb33f
Jason Barnett <J@sonBarnett.com>
2017-07-01 16:25:05 -0400

    Update microsoft_rsa.rb

--- lib/microsoft_rsa.rb
+++ lib/microsoft_rsa.rb
@@ -7,15 +7,19 @@ class MicrosoftRSA
   ELEMENTS.each do |x|
     attr_accessor x.downcase
   end
-
-  def self.load(xml)
-    file = File.new("mydoc.xml")
-    doc = REXML::Document.new file
-    elements = doc.elements.to_a("/*/*")
+  
+  def self.load(file)
+    ms_rsa = self.new
+    
+    f = File.new(file)
+    doc = REXML::Document.new(f)
+    
+    elements = doc.elements.to_a("/RSAKeyValue/*")
     elements.select! { |x| ELEMENTS.include?(x.name) }
     elements.each do |e|
-      e.name
-      e.text
+      ms_rsa.instance_variable_set(x.downcase, e.text)
     end
+    
+    ms_rsa
   end
 end

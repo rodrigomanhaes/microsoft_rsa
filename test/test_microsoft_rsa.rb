e9a4d4e1f7e14611cec945f020bea97afecce2f8
Jason Barnett <jason.w.barnett@gmail.com>
2017-07-03 06:58:58 -0400

    add tests

--- test/test_microsoft_rsa.rb
+++ test/test_microsoft_rsa.rb
@@ -1,7 +1,18 @@
-require 'helper'
+require 'minitest/autorun'
+require 'microsoft_rsa'
 
-class TestMicrosoftRsa < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+class TestMicrosoftRSA < Minitest::Test
+  KEY_FILE = "./test/fixtures/2048.xml"
+
+  def setup
+    @rsa = MicrosoftRSA.load(KEY_FILE)
+  end
+
+  def test_microsoft_rsa_class
+    assert_instance_of MicrosoftRSA, @rsa
+  end
+
+  def test_inspect_does_not_reveal_secrets
+    assert_equal @rsa.to_s, @rsa.inspect
   end
 end

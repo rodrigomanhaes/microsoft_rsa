db5ec96473d1d028366628a48925bf28f145a564
Jason Barnett <jason.w.barnett@gmail.com>
2017-07-04 10:01:26 -0400

    remove unused crap

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -25,12 +25,5 @@ rescue Bundler::BundlerError => e
   $stderr.puts "Run `bundle install` to install missing gems"
   exit e.status_code
 end
-require 'test/unit'
-require 'shoulda'
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'microsoft_rsa'
-
-class Test::Unit::TestCase
-end
